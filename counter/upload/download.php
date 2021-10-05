881a39073e0ab98514b82758c34999f3a47e468c
tantisolihat <48074326+tantisolihat@users.noreply.github.com>
2021-10-05 23:37:35 +0700

    Add files via upload

---
+++ counter/upload/download.php
@@ -0,0 +1,9 @@
+<?php
+$myDir = "D:/xampp/htdocs/counter/upload_file/";
+$dir = opendir($myDir);
+echo "Isi folder (klik link untuk download : <br>";
+while($tmp = readdir($dir)){
+	echo "<a href='$tmp' target='_blank'>$tmp</a><br>";
+}
+closedir($dir);
+?>
